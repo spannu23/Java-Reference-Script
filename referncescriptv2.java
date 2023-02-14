a6a8e7047dc0c6b07fe87b85d9fb59b1420e17cf
spannu23 <91493590+spannu23@users.noreply.github.com>
2023-02-14 11:46:33 -0800

    Add files via upload

--- referncescriptv2.java
+++ referncescriptv2.java
@@ -34,4 +34,39 @@ public class referncescriptv2 {
     System.out.println("The sum of the elements in the list is: " + sum);
     System.out.println("The count of iterations in the while loop is: " + count);
   }
-}
+  
+//method to sort an array of integers using bubble sort
+ public static void bubbleSort(int[] arr) {
+   int n = arr.length;
+   
+   // loop to traverse through all elements of the array
+   for (int i = 0; i < n-1; i++) {
+     
+     // loop to traverse the unsorted part of the array
+     for (int j = 0; j < n-i-1; j++) {
+       
+       // if current element is greater than the next element, swap them
+       if (arr[j] > arr[j+1]) {
+         int temp = arr[j];
+         arr[j] = arr[j+1];
+         arr[j+1] = temp;
+       }
+     }
+   }
+ }
+ 
+ // method to print the sorted array
+ public static void printArray(int[] arr) {
+   int n = arr.length;
+   for (int i=0; i<n; ++i)
+     System.out.print(arr[i] + " ");
+   System.out.println();
+ }
+ 
+ // main method to test the sorting algorithm
+ public static void main(String args[]) {
+   int[] arr = {64, 34, 25, 12, 22, 11, 90};
+   bubbleSort(arr);
+   System.out.println("Sorted array: ");
+   printArray(arr);
+ }
